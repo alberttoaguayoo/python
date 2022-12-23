1a0020cee7fd2d4443726ab37344d465213cec69
alberttoaguayoo <alberto@vauxoo.com>
2022-12-23 15:42:01 -0600

    pruba de edicion de archivo1

--- barman.py
+++ barman.py
@@ -21,8 +21,25 @@ def parseArguments():
 						help="cluster port.")
 	return parser.parse_known_args()
 
+def append_new_line(file_name, text_to_append):
+    """Append given text as a new line at the end of file"""
+    # Open the file in append & read mode ('a+')
+    with open(file_name, "a+") as file_object:
+        # Move read cursor to the start of file.
+        file_object.seek(0)
+        # If file is not empty then append '\n'
+        data = file_object.read(100)
+        if len(data) > 0:
+            file_object.write("\n")
+        # Append text at the end of file
+        file_object.write(text_to_append)
+
 args=parseArguments()
 print(args)
 
-
 subprocess.Popen('apt-get install -y barman', shell=True, stdin=None, stdout=None, stderr=None, executable="/bin/bash")
+
+path="/etc/postgresql/14/test/conf.d/00-deployv.conf"
+
+append_new_line(path, ArgumentParser("--client"))
+
