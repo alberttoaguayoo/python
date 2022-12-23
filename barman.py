904c7f61f2280c80448323d26388d937ce057ab9
alberttoaguayoo <alberto@vauxoo.com>
2022-12-22 19:21:15 -0600

    agregando barman instalacion

--- barman.py
+++ barman.py
@@ -11,14 +11,26 @@ def parseArguments():
 	parser.add_argument("--external", "-e",
 						type=str,
 						help="External ip address.")
+	parser.add_argument("--address", "-a",
+						type=str,
+						help="Barman ip address.")
+	parser.add_argument("--port", "-p",
+						type=str,
+						help="cluster port.")
 	return parser.parse_known_args()
 
 args=parseArguments()
 print(args)
-'''
-* Postgres cluster name.
-* External ip
-* Barman ip
-* Port
-* Customer id
-'''
+
+def package_installation(self):
+    self.apt = "apt "
+    self.ins = "install "
+    self.packages = "barman"
+
+    self.color.print_green("[+] Barman nstallation is starting:")
+
+    for self.items in self.packages.split():
+        self.command = str(self.apt) + str(self.ins) + str(self.items)
+
+        subprocess.run(self.command.split())
+        self.color.print_blue("\t[+] Package [{}] Installed".format(str(self.items)))
