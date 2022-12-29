380b23fb4cb94af0355fb4f89c91c20f47ac2783
alberttoaguayoo <alberto@vauxoo.com>
2022-12-28 20:56:54 -0600

    adding

--- barman.py
+++ barman.py
@@ -29,7 +29,7 @@ def parseArguments():
 def append_new_line(file_name, text_to_append):
     """Append given text as a new line at the end of file"""
     # Open the file in append & read mode ('a+')
-    with open(file_name, "a+") as file_object:
+    with open(file_name,"/conf.d/00-deployv.conf" ,"a+") as file_object:
         # Move read cursor to the start of file.
         file_object.seek(0)
         # If file is not empty then append '\n'
